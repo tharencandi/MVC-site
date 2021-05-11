aa779e5c73bd3dd8b922fdd5b04ef32973c58d96
Haeata <hbal7849@uni.sydney.edu.au>
2021-05-11 19:13:28 +1000

    sanitizer

--- code/sanitizer.py
+++ code/sanitizer.py
@@ -1,31 +1,64 @@
+import re
 
 class Sanitizer:
-    def __init__(self, char_dict=None, replace=False):
-        if not replace_char_dict:
-            self.char_dict= {
-                    "<": "&#x3C;", 
-                    ">": "&#x3E;",
-                    "/": ,
-                    ";": ,
-                    "\'": , 
-                    "\"": ,
-                    "\\": , 
-                    "&": ,
-                    "`": }
+    white_list_re = re.compile("[\w]*")
+
+    black_list = re.compile("(javascript|script|'|\")+", flags=re.IGNORECASE)
+
+    html_escapes = {"<": "&lt;", ">": "&gt;"}    
+
+    def __init__(self, replace=None, custom_black_list=None, custom_white_list=None):
+        if not replace or not isinstance(replace, dict):
+            self.replace = dict()
         else:
-            self.char_black_list = char_black_list
+            self.replace = replace
 
-        self.replace = replace
+        if custom_black_list:
+            self.black_list_re = re.compile(custom_black_list, flags=re.IGNORECASE)
+        else:
+            self.black_list_re = Sanitizer.black_list
 
-    def block(self, string):
+        if custom_white_list:
+            self.white_list_re = re.compile(custom_white_list, flags=re.IGNORECASE)
+        else:
+            self.white_list_re = Sanitizer.white_list_re
+
+        
+
+
+    def only_white_list(self, string):
         if not string:
             return None
+        if not self.white_list_re.fullmatch(string):
+            return False
+        return True
+    
+    def contains_black_list(self, string):
+        if not string:
+            return None
+        if self.black_list_re.match(string):
+            return True
+        else:
+            return False
+    
+    def sanitize(self, string, only_alpha_num=False):
+        if not string:
+            return None
+        if self.contains_black_list(string):
+            return None
 
-        for char in string:
-            if char in self.char_black_list:
+        if only_alpha_num:
+            if not self.only_white_list(string):
                 return None
+
+        for k in Sanitizer.html_escapes:
+            string = string.replace(k, Sanitizer.html_escapes[k])
+
+        for k in self.replace:
+            string = string.replace(k, self.replace[k])
 
         return string
 
-    def replace_bad_chars(self, string):
-        for 
+
+
+
